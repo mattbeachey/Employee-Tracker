c052fc24cc703131b17e20248abe74ef397ba6a8
Matt Beachey <mattbeachey@gmail.com>
2019-12-19 01:34:07 -0600

    build and seeded databases

---
+++ schema.sql
@@ -0,0 +1,37 @@
+DROP DATABASE IF EXISTS employee_tracker_DB;
+
+CREATE DATABASE employee_tracker_DB;
+
+USE employee_tracker_DB;
+
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE roles (
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30),
+    salary  DECIMAL (10,2),
+    department_id INT,
+    PRIMARY kEY (id)
+);
+
+CREATE TABLE employees (
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT, 
+    manager_id INT,
+    PRIMARY KEY (id)
+)
+
+INSERT INTO employees (id, first_name, last_name, role_id, manager_id)
+VALUES (id, Matt, Beachey)
+
+INSERT INTO
+VALUES
+
+INSERT INTO
+VALUES
---
+++ seed.sql
@@ -0,0 +1,32 @@
+
+
+
+USE employee_tracker_DB;
+INSERT INTO department (name)
+VALUES ('Accounts Receivable');
+INSERT INTO department (name)
+VALUES ('HR');
+INSERT INTO department (name)
+VALUES ('Operations');
+INSERT INTO department (name)
+VALUES ('R&D');
+INSERT INTO department (name)
+VALUES ('Strawberry');
+INSERT INTO roles (title, salary, department_id)
+VALUES ("Employee",1000000, 5);
+INSERT INTO roles (title, salary, department_id)
+VALUES ("Liason",900000, 2);
+INSERT INTO roles (title, salary, department_id)
+VALUES ("Manager",20000, 1);
+INSERT INTO roles (title, salary, department_id)
+VALUES ("Vice President",2000000, 5);
+INSERT INTO employees (first_name , last_name , role_id, manager_id)
+VALUES ("Bill", "Joeman", 3, NULL);
+INSERT INTO employees (first_name , last_name , role_id, manager_id)
+VALUES ("Paul", "Hendrickson", 4, 1);
+INSERT INTO employees (first_name , last_name , role_id, manager_id)
+VALUES ("Billy", "Jean", 2, 1);
+INSERT INTO employees (first_name , last_name , role_id, manager_id)
+VALUES ("Frank", "Huricane", 1, 1);
+INSERT INTO employees (first_name , last_name , role_id, manager_id)
+VALUES ("Matt", "Beachey", 5, 1);
