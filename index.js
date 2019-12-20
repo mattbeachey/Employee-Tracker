5c893471a126dfb59e5071dc635a53bf40a646e7
Matt Beachey <mattbeachey@gmail.com>
2019-12-20 01:18:12 -0600

    all add queries working

--- index.js
+++ index.js
@@ -15,10 +15,9 @@ const seeds = require("./seeds")
 
 
 // model dependencies
-// const Roles = require ("./models/roles")
-// const Departments = require ("./models/departments")
-// const Employees = require ("./models/employees")
 const db = require("./models")
+
+//syncing with database
 db.sequelize.sync({ force: false }).then(function () {
     // db.departments.create({name: "development"}).then(function(){
     //     db.departments.findAll({}).then(function(res){
@@ -29,12 +28,12 @@ db.sequelize.sync({ force: false }).then(function () {
     startingPrompt();
 })
 
-// console.log(db)
 
 function startingPrompt() {
     const view = "View departments, roles, or employees";
     const add = "Add departments, roles, or employees";
     const update = "Update employee roles"
+    console.log("///////////////////////////////////////////////////////////////////////////////////////")
 
     inquirer.prompt([
         {
@@ -59,7 +58,7 @@ function startingPrompt() {
                     break;
 
                 case "Quit":
-                    
+
                     return;
             }
         })
@@ -80,7 +79,7 @@ function viewDb() {
             db[userSelection].findAll({}).then(function (res) {
                 const nameArray = []
                 res.forEach(element => {
-                    name = element.dataValues 
+                    name = element.dataValues
                     nameArray.push(name)
                 });
                 console.table(nameArray)
@@ -96,12 +95,108 @@ function addToDb() {
             type: "list",
             name: "userChoice",
             message: "Which database would you like to add to?",
-            choices: ["Departments", "Roles", "Employees"]
+            choices: ["departments", "roles", "employees", "return to main menu"]
         }
     ])
+
         .then(function (data) {
-            console.log("adding to database: " + data.userChoice)
-        });
+            switch (data.userChoice) {
+                case "departments":
+                    addDepartment()
+                    break;
+
+                case "roles":
+                    addRole()
+                    break;
+
+                case "employees":
+                    addEmployee()
+                    break;
+
+                case "return to main menu":
+                    startingPrompt()
+                    return;
+            }
+        })
+
+}
+
+
+function addDepartment() {
+    inquirer.prompt([
+        {
+            message: "What is the name of this new department?",
+            name: "userChoice"
+        }
+    ])
+
+        .then(function (data) {
+            userSelection = data.userChoice
+            db.departments.create({ name: `${userSelection}` }).then(function () {
+                console.log("added " + userSelection)
+            })
+        })
+}
+
+function addEmployee() {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "first_name",
+            message: "What is the Employee's First Name?"
+        },
+        {
+            type: "input",
+            name: "last_name",
+            message: "What is the Employee's Last Name?" 
+        },
+        {
+            type: "input",
+            name: "role_id",
+            message: "What is the Employees Role Id?"
+        },
+        {
+            type: "input",
+            name: "manager_id",
+            message: "What is the Employees Manager Id?"
+        }
+    ])
+
+        .then(function (data) {
+            userSelection = [data.first_name, data.last_name, data.role_id, data.manager_id]
+            console.log("added " + userSelection)
+            db.employees.create({ first_name: `${data.first_name}`, last_name: `${data.last_name}`, role_id: `${data.role_id}`, manager_id: `${data.manager_id}` }).then(function () {
+                console.log("added " + userSelection)
+            })
+        })
+}
+
+function addRole() {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "title",
+            message: "What is this new role's name?"
+        },
+        {
+            type: "input",
+            name: "salary",
+            message: "What is this new role's salary?" 
+        },
+        {
+            type: "input",
+            name: "department_id",
+            message: "What is this new role's department ID?"
+        }
+    ])
+
+        .then(function (data) {
+            userSelection = [data.title, data.salary, data.department_id, data.manager_id]
+            console.log("added " + userSelection)
+            db.roles.create({ title: `${data.title}`, salary: `${data.salary}`, department_id: `${data.department_id}` }).then(function () {
+                console.log("added " + userSelection)
+            })
+        })
 }
 
 function updateDb() {
